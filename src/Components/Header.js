227476aec3f6f53e632022d129112227d78794ca
Nicolas Vitry <74904811+Nv974@users.noreply.github.com>
2022-02-18 10:40:39 +0400

    integration login page and navbar

--- src/Components/Header.js
+++ src/Components/Header.js
@@ -1,34 +1,62 @@
 import React, { useState } from 'react';
 import { NavLink } from 'react-router-dom';
-
+import brand from '../assets/images/branding.png';
 
 const Header = () => {
+    const [collapse, setCollapsed] = useState(true);
 
-	const [collapse, setCollapsed] = useState(true);
+    const handleCollapse = () => setCollapsed(!collapse);
 
-	const handleCollapse = () => setCollapsed(!collapse);
-
-	return (
-		<header>
-			<nav className="navbar navbar-expand-md navbar-dark fixed-top bg-dark">
-				<div className="container-fluid">
-					<button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarCollapse" aria-controls="navbarCollapse" aria-expanded={!collapse ? true : false} aria-label="Toggle navigation" onClick={handleCollapse}>
-						<span className="navbar-toggler-icon"></span>
-					</button>
-					<div className={`${collapse ? 'collapse' : ''} navbar-collapse`} id="navbarCollapse">
-						<ul className="navbar-nav me-auto mb-2 mb-md-0">
-							<li className="nav-item">
-								<NavLink exact={true} className="nav-link" to="/">Home</NavLink>
-							</li>
-							<li className="nav-item">
-								<NavLink exact={true} className="nav-link" to="/private">Private</NavLink>
-							</li>
-						</ul>
-					</div>
-				</div>
-			</nav>
-		</header>
-	);
+    return (
+        <header>
+            <nav className='navbar navbar-expand-md navbar-dark p-3'>
+                <div className='container-fluid'>
+                    <div className='navbar-brand'>
+                        <img src={brand} alt='logo' className='p-0 m-0' />
+                    </div>
+                    <button
+                        className='navbar-toggler'
+                        type='button'
+                        data-bs-toggle='collapse'
+                        data-bs-target='#navbarCollapse'
+                        aria-controls='navbarCollapse'
+                        aria-expanded={!collapse ? true : false}
+                        aria-label='Toggle navigation'
+                        onClick={handleCollapse}
+                    >
+                        <span className='navbar-toggler-icon'></span>
+                    </button>
+                    <div
+                        className={`${
+                            collapse ? 'collapse' : ''
+                        } navbar-collapse`}
+                        id='navbarCollapse'
+                    >
+                        <ul className='navbar-nav mx-auto'>
+                            <li className='nav-item'>
+                                <NavLink
+                                    exact={true}
+                                    className='nav-link'
+                                    to='/'
+                                >
+                                    Home
+                                </NavLink>
+                            </li>
+                            <li className='nav-item'>
+                                <NavLink
+                                    exact={true}
+                                    className='nav-link'
+                                    to='/private'
+                                >
+                                    Private
+                                </NavLink>
+                            </li>
+                        </ul>
+                    </div>
+                </div>
+            </nav>
+        </header>
+    );
 };
 
 export default Header;
--- src/Screens/Login.js
+++ src/Screens/Login.js
@@ -1,22 +1,83 @@
 import React, { lazy } from 'react';
+import loginBg from '../assets/images/login.png';
 
 const Layout = lazy(() => import('../Components/Layout'));
 
 const Login = () => {
-	return (
-		<Layout title="Connexion">
-			<div className="container">
-				<form className="form-signin">
-					<h1 className="h3 mb-3 fw-normal">Veuillez vous connecter</h1>
-					<label htmlFor="inputEmail" className="mt-3">Email</label>
-					<input type="email" id="inputEmail" className="form-control" placeholder="Email" required="" autoFocus="" />
-					<label htmlFor="inputPassword" className="mt-3">Mot de passe</label>
-					<input type="password" id="inputPassword" className="form-control" placeholder="Mot de passe" required="" />
-					<button className="w-100 btn btn-lg btn-primary mt-3" type="submit">Sign in</button>
-				</form>
-			</div>
-		</Layout>
-	);
+    return (
+        <Layout title='Connexion'>
+            <div className='row vh-100 m-0'>
+                <div className='col-xl-7 col-md-6 my-auto'>
+                    <div className='container'>
+                        <form className='form-signin'>
+                            <h1 className='h3 mb-3 fw-normal'>Connexion</h1>
+                            <p>
+                                Vous avez déja un compte ?
+                                <span className='text-decoration-underline mx-1 text-second'>
+                                    Inscription
+                                </span>
+                            </p>
+                            <label
+                                htmlFor='inputEmail'
+                                className='visually-hidden'
+                            >
+                                Email
+                            </label>
+                            <input
+                                type='email'
+                                id='inputEmail'
+                                className='form-control custom-input'
+                                placeholder='Email'
+                                required=''
+                                autoFocus=''
+                            />
+                            <label
+                                htmlFor='inputPassword'
+                                className='visually-hidden'
+                            >
+                                Mot de passe
+                            </label>
+                            <input
+                                type='password'
+                                id='inputPassword'
+                                className='form-control custom-input mt-3'
+                                placeholder='Mot de passe'
+                                required=''
+                            />
+
+                            <small
+                                className='fw-bold'
+                                style={{
+                                    fontSize: 12,
+                                    ligneHeight: 20,
+                                }}
+                            >
+                                Bégaiement s'engage à respecter vos données. En
+                                continuant votre inscription, vous acceptez nos
+                                conditions générales.
+                            </small>
+
+                            <button
+                                className='w-100 btn btn-purple text-white mt-3 p-2 '
+                                type='submit'
+                            >
+                                Accéder à mon espace
+                            </button>
+                        </form>
+                    </div>
+                </div>
+                <div
+                    className='col-xl-5 col-md-6'
+                    style={{
+                        backgroundImage: `url(${loginBg})`,
+                        backgroundPosition: 'center center',
+                        backgroundSize: 'cover',
+                        backgroundRepeat: 'no-repeat',
+                    }}
+                ></div>
+            </div>
+        </Layout>
+    );
 };
 
 export default Login;
