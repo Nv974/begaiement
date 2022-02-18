ca63675bcab513a9c6ecb9d0f6f8d70015f86e54
Nicolas Vitry <74904811+Nv974@users.noreply.github.com>
2022-02-18 15:40:27 +0400

    display content-list

---
+++ src/datas/categories.js
@@ -0,0 +1,56 @@
+const categories = [
+    {
+        categoryId: 1,
+        title: 'Bilan par âge',
+        videos: [
+            {
+                videoId: 1,
+                title: 'Critère de chonicisation',
+                author: 'Aude Fresnay',
+            },
+            {
+                videoId: 2,
+                title: 'Titre#2',
+                author: 'Aude Fresnay',
+            },
+            {
+                videoId: 3,
+                title: 'Titre#3',
+                author: 'Aude Fresnay',
+            },
+            {
+                videoId: 4,
+                title: 'Titre #4',
+                author: 'Aude Fresnay',
+            },
+        ],
+    },
+    {
+        categoryId: 2,
+        title: 'Conduire un partenariat parental',
+        videos: [
+            {
+                videoId: 5,
+                title: 'Critère de chonicisation',
+                author: 'Aude Fresnay',
+            },
+            {
+                videoId: 6,
+                title: 'Titre#2',
+                author: 'Aude Fresnay',
+            },
+            {
+                videoId: 7,
+                title: 'Titre#3',
+                author: 'Aude Fresnay',
+            },
+            {
+                videoId: 8,
+                title: 'Titre #4',
+                author: 'Aude Fresnay',
+            },
+        ],
+    },
+];
+
+export default categories;
